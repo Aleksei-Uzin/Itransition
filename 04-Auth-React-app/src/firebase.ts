ab2e418de3fbdf92cbc53313cd64fa08e5e97d2c
Aleksei-Uzin <aleksei.uzin@gmail.com>
2024-09-18 18:17:53 +0400

    init and setting up: firebase authentication

---
+++ 04-Auth-React-app/src/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA4c_AdXy0wgMzhFBXHK9RFjAZ_68QbmDI',
+  authDomain: 'itransition-task4-auth-app.firebaseapp.com',
+  databaseURL:
+    'https://itransition-task4-auth-app-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'itransition-task4-auth-app',
+  storageBucket: 'itransition-task4-auth-app.appspot.com',
+  messagingSenderId: '703453506135',
+  appId: '1:703453506135:web:1da1f63f662853e9a01112',
+}
+
+export const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
