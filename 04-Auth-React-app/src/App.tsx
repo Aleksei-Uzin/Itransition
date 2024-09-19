4a64df7364eea1eac43b69fbca68a5f279f029a4
Aleksei-Uzin <aleksei.uzin@gmail.com>
2024-09-19 13:33:19 +0400

    create: App component

---
+++ 04-Auth-React-app/src/App.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import {
+  AuthenticatedApp,
+  FullPageSpinner,
+  UnauthenticatedApp,
+} from './components'
+import { auth, STATUS } from './api'
+import { onAuthStateChanged, User } from 'firebase/auth'
+
+function App() {
+  const [user, setUser] = useState<User | null>(null)
+  const [status, setStatus] = useState<STATUS>(STATUS.IDLE)
+  const handleSetUser = (newUser: User | null) => setUser(newUser)
+
+  useEffect(() => {
+    onAuthStateChanged(auth, signedUser => {
+      setUser(signedUser)
+      setStatus(STATUS.RESOLVED)
+    })
+  }, [])
+
+  if (status === STATUS.IDLE || status === STATUS.PENDING) {
+    return <FullPageSpinner />
+  }
+
+  if (status === STATUS.RESOLVED) {
+    return user ? (
+      <AuthenticatedApp user={user} setUser={handleSetUser} />
+    ) : (
+      <UnauthenticatedApp setUser={handleSetUser} />
+    )
+  }
+}
+
+export default App
