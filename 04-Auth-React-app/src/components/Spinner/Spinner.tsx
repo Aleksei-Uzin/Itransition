af1cb05475a9d0896f6db7ef5228fffbbe8ac809
Aleksei-Uzin <aleksei.uzin@gmail.com>
2024-09-19 15:13:33 +0400

    fix: Spinner size

--- 04-Auth-React-app/src/components/Spinner/Spinner.tsx
+++ 04-Auth-React-app/src/components/Spinner/Spinner.tsx
@@ -1,5 +1,5 @@
 export const Spinner = () => (
-  <div role="status">
+  <span role="status">
     <svg
       aria-hidden="true"
       className="inline w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
@@ -17,5 +17,5 @@ export const Spinner = () => (
       />
     </svg>
     <span className="sr-only">Loading...</span>
-  </div>
+  </span>
 )
